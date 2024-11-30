47c5aea6e37a625e3f364bfbe2f23624d8518175
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-11-30 19:15:14 +0200

    add base stucture and middlewares

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ import morgan from "morgan";
 import cors from "cors";
 import "dotenv/config";
 import "./bd.js";
+import YAML from "yamljs";
+import SwaggerUI from "swagger-ui-express";
+
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 const app = express();
+
+app.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(swaggerDocument));
 
 app.use(morgan("tiny"));
 app.use(cors());
