d11db42440f4b72bf605574119820b546fe606e9
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-12-12 22:03:28 +0200

    added websockets connection to enable realtime messaging

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ import morgan from "morgan";
 import cors from "cors";
 import "dotenv/config";
 import "./bd.js";
+import http from "http";
+import { Server } from "socket.io";
 import YAML from "yamljs";
 import SwaggerUI from "swagger-ui-express";
 
 import chatsRouter from "./routes/chatsRouter.js";
 import messagesRouter from "./routes/messagesRouter.js";
+
+import { messages } from "./models/messages.js";
 
 const swaggerDocument = YAML.load("./swagger.yaml");
 
@@ -22,10 +26,36 @@ app.use(express.json());
 app.use("/api/chats", chatsRouter);
 app.use("/api/messages", messagesRouter);
 
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "https://npavl-chat.netlify.app/",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("sendMessage", async (messageData) => {
+    try {
+      const newMessage = await messages.create(messageData);
+      io.to(messageData.chatId).emit("newMessage", newMessage);
+    } catch (e) {
+      console.error(e.message);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-app.listen(8080, () => {
+server.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
